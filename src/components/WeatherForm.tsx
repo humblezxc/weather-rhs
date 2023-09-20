2c3c076706b1cfa095dad1314de22939048fa47e
humblezxc <stopenchuk.yaroslav@gmail.com>
2023-09-20 14:44:56 +0200

    Created weather form submit

--- src/components/WeatherForm.tsx
+++ src/components/WeatherForm.tsx
@@ -1,7 +1,21 @@
-export default function WeatherForm() {
+import React, {useState} from "react";
+import {IHandleCityChange} from "../models.ts";
+
+interface WeatherFormProps {
+    handleCityChange: IHandleCityChange
+}
+export default function WeatherForm({handleCityChange}: WeatherFormProps) {
+    const [city, setCity] = useState('')
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setCity(e.target.value.trim())
+    }
+    const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+      e.preventDefault();
+        handleCityChange(city);
+    }
     return (
-        <form className="flex pb-4">
-            <input className="border focus:border-red-600 outline-none p-2" type="text" placeholder="Enter city name"/>
+        <form className="flex pb-4" onSubmit={handleSubmit}>
+            <input className="border focus:border-red-600 outline-none p-2" onChange={handleChange} type="text" placeholder="Enter city name"/>
             <button className="button bg-red-600">Show</button>
         </form>
     )
