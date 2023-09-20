fa2d156705188b038d01da25d170202a418fac9b
humblezxc <stopenchuk.yaroslav@gmail.com>
2023-09-20 14:46:11 +0200

    lifted handleCityChange to parent component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.scss'
 import Header from "./components/Header.tsx";
-import WeatherLayout from "./components/WeatherLayout.tsx";
+import MainContent from "./components/MainContent.tsx";
+import {useState} from "react";
 
 function App() {
+    const [city, setCity] = useState('');
+    const handleCityChange = (newCity: string) => {
+        setCity(newCity);
+    };
     return (
         <>
-            <Header/>
-            <WeatherLayout />
+            <Header handleCityChange={handleCityChange}/>
+            <MainContent city={city}/>
 
         </>
     )
---
+++ src/models.ts
@@ -0,0 +1,1 @@
+export type IHandleCityChange = (city: string) => void;
